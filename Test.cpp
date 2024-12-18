87dcaa339dad3f142f76aba256ed8608f46284da
gaoshi <258775211@qq.com>
2024-12-19 00:53:56 +0800

    程序验证-更新

--- Test.cpp
+++ Test.cpp
@@ -1,11 +1,11 @@
 #include <Windows.h>
 #include <iostream>
 #include <memory>
+#include <thread>
 #include "Win32/WindowFactory.h"
 #include "Render/RenderFactory.h"
 
 using namespace std;
-
 
 int APIENTRY wWinMain(
     _In_        HINSTANCE hInstance,
@@ -13,13 +13,9 @@ int APIENTRY wWinMain(
     _In_        LPWSTR    lpCmdLine,
     _In_        int       nCmdShow)
 {
-
     WindowFactory::SethInstance(hInstance);
     auto mainWindow = WindowFactory::Build(250, 80, 640, 480);
     if (mainWindow) {
-        RenderFactory::InitRender(RenderFactory::RenderType_Direct2D);
-        auto render = RenderFactory::GetInstance();
-
         mainWindow->Process();
     }
 
