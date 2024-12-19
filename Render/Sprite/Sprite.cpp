53b73dce778c6c49359d9d1472753a9b5eb413d8
gaoshi <258775211@qq.com>
2024-12-19 13:51:37 +0800

    修改精灵类-增加渲染文本逻辑

--- Render/Sprite/Sprite.cpp
+++ Render/Sprite/Sprite.cpp
@@ -2,13 +2,47 @@
 #include "../RenderFactory.h"
 #include "../Direct2D/Render_Direct2d.h"
 
+// 多字节转为宽字符 
+static bool CharToWChar(const char* szStr, WCHAR* wszClassName) {
+	bool result = false;
+	if (wszClassName != nullptr) {
+		int wlen = 0, len = (int)strlen(szStr);
+
+		// 转换 
+		wlen = MultiByteToWideChar(CP_ACP, 0, szStr, len, wszClassName, 0);
+		result = MultiByteToWideChar(CP_ACP, 0, szStr, len, wszClassName, wlen);
+		if (result != 0) {
+			wszClassName[len] = '\0';
+		}
+	}
+	return result;
+}
+
 // 构造
 Sprite::Sprite() {
+	// 图像属性
 	_image = nullptr;
-	draw_x = draw_y = draw_width = draw_height = 0;
-	src_x = src_y = src_width = src_height = 0;
 	opacity = 1.0f;
 	angle = 0.0f;
+	src_x = src_y = src_width = src_height = 0;
+
+
+	// 文本属性
+	text = "";					// 文字
+	render_text = nullptr;		// 渲染用文字
+	font_size = 18.0f;			// 文字大小
+	text_format = nullptr;		// 文字格式
+	text_layout = nullptr;		// 文本布局 
+	text_brash = nullptr;		// 底子画刷 
+	memset(text_color, 0, sizeof(int) * 3);	// 画刷颜色
+	max_fontlength = 1024;		// 可绘制文本长度
+	text_HorizontalStyle = 1;	// 文本水平风格
+	text_VerticalStyle = 1;		// 文本垂直风格
+	text_swrapping = 0;			// 换行格式
+
+	// 绘制区
+	draw_x = draw_y = draw_width = draw_height = 0;
+
 }
 
 // 析构
@@ -21,7 +55,7 @@ Sprite::~Sprite() {
 
 }
 
-// 创建
+// 创建图像
 void Sprite::SetImage(const char* filename) {
 	auto render = RenderFactory::GetInstance()->GetRender();
 	if (filename && render && !this->_image) {
@@ -32,6 +66,26 @@ void Sprite::SetImage(const char* filename) {
 		draw_width = draw_x + src_width;
 		draw_height = draw_y + src_height;
 	}
+}
+
+// 创建文本
+void Sprite::SetText(const std::string& _text) {
+	// 重新设置文本
+	this->text.clear();
+	this->text = _text;
+	if (this->render_text) {
+		delete this->render_text;
+		this->render_text = nullptr;
+	}
+
+	// 字节转换
+	size_t loopnum = text.size() / limit_length;
+	this->render_text = new wchar_t[limit_length * (loopnum + 1)];
+	memset(this->render_text, '\0', sizeof(wchar_t) * (limit_length * (loopnum + 1)));
+	CharToWChar(this->text.c_str(), this->render_text);
+
+	// 重设文本布局
+	
 }
 
 // 更新
@@ -45,8 +99,36 @@ void Sprite::Draw() {
 	if (_image) {
 		auto render = RenderFactory::GetInstance()->GetRender();
 		if (render) {
+			// 图像绘制
 			render->RenderImage(_image, draw_x, draw_y, draw_width, draw_height, src_x, src_y, src_width, src_height, opacity, angle);
+
+			// 图形绘制
+
+			// 线条绘制
+
+			// 文本绘制
+
 		}
 	}
+
+}
+
+// 绘制图像
+void Sprite::DrawImage() {
+
+}
+
+// 单独绘制图形
+void Sprite::DrawFigure() {
+
+}
+
+// 单独绘制线条
+void Sprite::DrawLine() {
+
+}
+
+// 单独绘制文本
+void Sprite::DrawText() {
 
 }
--- Render/Sprite/Sprite.h
+++ Render/Sprite/Sprite.h
@@ -1,17 +1,36 @@
 #ifndef _Sprite_h_
 #define _Sprite_h_
 
-#include "../Render.h"
+#include "../Render.h"	// 渲染器
+#include <iostream>		// 标准组件
+
+const int limit_length = 1024;
 
 // 精灵类
 class Sprite {
 private:
-	void* _image;	// 图像对象
-	float opacity;	// 不透明度
-	float angle;	// 旋转角度
-	int draw_x, draw_y, draw_width, draw_height;	// 绘制区
-	int src_x, src_y, src_width, src_height;		// 资源裁剪区
+	// 图像
+	void* _image;				// 图像对象
+	float opacity;				// 不透明度
+	float angle;				// 旋转角度
+	int src_x, src_y, 
+		src_width, src_height;	// 资源裁剪区
 
+	// 文本
+	std::string text;			// 文字
+	wchar_t* render_text;		// 渲染用
+	float font_size;			// 文字大小
+	void* text_format;			// 文字格式
+	void* text_layout;			// 文本布局 
+	void* text_brash;			// 底子画刷 
+	int text_color[3];			// 画刷颜色
+	int max_fontlength;			// 可绘制文本长度
+	int text_HorizontalStyle;	// 文本水平风格 - 1 默认居中
+	int text_VerticalStyle;		// 文本垂直风格 - 1 默认居中
+	int text_swrapping;			// 换行格式
+
+	// 绘制区
+	int draw_x, draw_y, draw_width, draw_height;
 
 public:
 	// 构造
@@ -20,14 +39,30 @@ public:
 	// 析构
 	~Sprite();
 
-	// 创建
+	// 创建图像
 	void SetImage(const char* filename = nullptr);
+
+	// 创建文本
+	void SetText(const std::string& _text);
 
 	// 更新
 	void Update();
 
 	// 绘制
+	// 图像、图形、线条、文本
 	void Draw();
+
+	// 单独绘制图像
+	void DrawImage();
+
+	// 单独绘制图形
+	void DrawFigure();
+
+	// 单独绘制线条
+	void DrawLine();
+
+	// 单独绘制文本
+	void DrawText();
 
 };
 
