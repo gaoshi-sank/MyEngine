e7a845fc59a6d6fec998db5a194be50e8c49d237
gaoshi <258775211@qq.com>
2024-12-19 13:29:37 +0800

    增加-精灵类

--- Render/Sprite/Sprite.cpp
+++ Render/Sprite/Sprite.cpp
@@ -1,1 +1,52 @@
 #include "Sprite.h"
+#include "../RenderFactory.h"
+#include "../Direct2D/Render_Direct2d.h"
+
+// 构造
+Sprite::Sprite() {
+	_image = nullptr;
+	draw_x = draw_y = draw_width = draw_height = 0;
+	src_x = src_y = src_width = src_height = 0;
+	opacity = 1.0f;
+	angle = 0.0f;
+}
+
+// 析构
+Sprite::~Sprite() {
+
+	if (_image) {
+		delete _image;
+		_image = nullptr;
+	}
+
+}
+
+// 创建
+void Sprite::SetImage(const char* filename) {
+	auto render = RenderFactory::GetInstance()->GetRender();
+	if (filename && render && !this->_image) {
+		this->_image = render->CreateImage(filename);
+
+		// 默认全部一起绘制，不做裁剪
+		render->GetImageSize(this->_image, src_width, src_height);
+		draw_width = draw_x + src_width;
+		draw_height = draw_y + src_height;
+	}
+}
+
+// 更新
+void Sprite::Update() {
+
+}
+
+// 绘制
+void Sprite::Draw() {
+
+	if (_image) {
+		auto render = RenderFactory::GetInstance()->GetRender();
+		if (render) {
+			render->RenderImage(_image, draw_x, draw_y, draw_width, draw_height, src_x, src_y, src_width, src_height, opacity, angle);
+		}
+	}
+
+}
--- Render/Sprite/Sprite.h
+++ Render/Sprite/Sprite.h
@@ -5,7 +5,29 @@
 
 // 精灵类
 class Sprite {
+private:
+	void* _image;	// 图像对象
+	float opacity;	// 不透明度
+	float angle;	// 旋转角度
+	int draw_x, draw_y, draw_width, draw_height;	// 绘制区
+	int src_x, src_y, src_width, src_height;		// 资源裁剪区
 
+
+public:
+	// 构造
+	Sprite();
+
+	// 析构
+	~Sprite();
+
+	// 创建
+	void SetImage(const char* filename = nullptr);
+
+	// 更新
+	void Update();
+
+	// 绘制
+	void Draw();
 
 };
 
