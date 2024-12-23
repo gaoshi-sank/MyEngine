aecaeaef5df5a96106a94544f026a013a5ff70b1
gaoshi <258775211@qq.com>
2024-12-23 13:17:00 +0800

    增加UI-几个回调函数

--- Umg/Control/UI_Lable.h
+++ Umg/Control/UI_Lable.h
@@ -9,6 +9,11 @@ private:
 	SImage* _image;	// 图像
 	SText* _text;	// 文本
 
+	// 事件回调指针
+	std::function<void(int* _param)> callback_hover;	// 悬停回调
+	std::function<void(int* _param)> callback_down;		// 按下回调
+	std::function<void(int* _param)> callback_up;		// 放开回调
+	std::function<void(int* _param)> callback_click;	// 点击回调
 
 public:
 	// 构造
@@ -47,6 +52,18 @@ public:
 
 	// 设置可见性
 	virtual void SetVisiable(bool visible = false);
+
+	// 设置回调 - 悬停
+	virtual void Event_Hover(std::function<void(int* _param)> _hover);
+
+	// 设置回调 - 按下
+	virtual void Event_Down(std::function<void(int* _param)> _down);
+
+	// 设置回调 - 放开
+	virtual void Event_Up(std::function<void(int* _param)> _up);
+
+	// 设置回调 - 点击
+	virtual void Event_Click(std::function<void(int* _param)> _hover);
 };
 
 
