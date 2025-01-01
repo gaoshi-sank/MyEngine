82b22e39b60fc4550bdc62d367955b8e9348c8a2
gaoshi <258775211@qq.com>
2025-01-02 00:13:20 +0800

    修改验证方式

--- Test/Scene_Test.cpp
+++ Test/Scene_Test.cpp
@@ -16,24 +16,6 @@ Scene_Test::~Scene_Test() {
 void Scene_Test::Init() {
 	std::string testpath = "C:\\Users\\asus\\Pictures\\39\\Common\\9-1.png";
 	
-	_test = new UI_Plane();
-	if (_test) {
-		_test->SetLocation(0, 0);
-		_test->SetSize(200, 50);
-
-		UI_Button* att1 = new UI_Button();
-		if (att1) {
-			att1->Create(testpath.c_str(), 0, 0, 100, 50);
-			_test->AddAttachedUI(att1);
-		}
-		
-		UI_Button* att2 = new UI_Button();
-		if (att2) {
-			att2->Create(testpath.c_str(), 100, 0, 100, 50);
-			_test->AddAttachedUI(att2);
-		}
-	}
-	
 }
 
 //  Õ∑≈
